e9507bf96f8729193c3b67893292f35ccb60c7ff
Loyd Stewart 2 <loydstewart2@icloud.com>
2019-01-16 15:35:49 -0500

    signUp from signIn

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 var companiesRouter = require('./routes/companies');
+var signUpRouter = require('./routes/signUp');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 app.use('/companies', companiesRouter);
+app.use('/signUp', signUpRouter);
 // app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
