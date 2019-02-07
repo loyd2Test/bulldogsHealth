def6c8a2a635545b87f94ebd9c58d02dd12217cc
Loyd Stewart 2 <loydstewart2@icloud.com>
2019-02-07 10:38:21 -0500

    form complete + password check

--- public/javascripts/checkPassword.js
+++ public/javascripts/checkPassword.js
@@ -2,13 +2,36 @@ function checkPasswordMatch() {
     var password = $("#p1").val();
     var confirmPassword = $("#p2").val();
     // console.log(password)
-
-    if (password != confirmPassword)
-    $('#divCheckPasswordMatch').text("Passwords do not match!");
-    else
-    $('#divCheckPasswordMatch').text("Passwords match.");
+    if(password && confirmPassword == '')
+    $('#divCheckPasswordMatch').text("Please enter a password");
+    if (password != confirmPassword){
+        $('#divCheckPasswordMatch').text("Passwords do not match!");
+    $("input[type=submit]").prop("disabled", true);}
+    
+    else{$('#divCheckPasswordMatch').text("Passwords match.");
+    $("input[type=submit]").prop("disabled", false);}
+    
 }
 
 $(document).ready(function () {
     $("#p2").keyup(checkPasswordMatch);
 });
+
+
+// $(document).ready(function (){
+//     validate();
+//     $('#f, #l, #m, #p1, #p2').change(validate);
+// });
+
+// function validate(){
+//     if ($('#f').val().length   >   0   &&
+//         $('#l').val().length  >   0   &&
+//         $('#m').val().length  >   0   &&
+//         $('#p1').val().length  >   0   &&
+//         $('#p2').val().length    >   0) {
+//         $("input[type=submit]").prop("disabled", false);
+//     }
+//     else {
+//         $("input[type=submit]").prop("disabled", true);
+//     }
+// }
