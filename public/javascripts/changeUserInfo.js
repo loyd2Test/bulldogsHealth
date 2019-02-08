70c802a0f7b9cfff02edc7d5792196d54769209b
Loyd Stewart 2 <loydstewart2@icloud.com>
2019-02-08 00:04:12 -0500

    idk

---
+++ public/javascripts/changeUserInfo.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+
+function change() {
+    var id = document.getElementById("me").getAttribute("data-1");
+    $('#hi').text(id);
+    // var me = user;
+    // console.log(me);
+    models.Candidate.findOne({where:{id: id}}).then(function(user) {
+    //   res.render('user', {user});
+        $('#bye').text(user.firstName);
+    });
+
+    
+}
+
+$(document).ready(function () {
+    $("#checkIt").onclick(change);
+});
+
+module.exports = router;
