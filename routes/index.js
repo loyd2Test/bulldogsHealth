dba51246e1e34740eff9db7966e410400d39b79c
Loyd Stewart 2 <loydstewart2@icloud.com>
2019-02-10 19:56:48 -0500

    more start screen updates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('start', { title: 'Express' });
+   models.Job.findAll({where:{id: [1,2,3,4,5,6,7,8,9,10]}}).then(function(jobs){
+    res.render('start', {jobs: jobs, title: 'Express' });
+   });
+  
 });
 
 module.exports = router;
